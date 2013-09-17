d98cc5dc9f2f8bd3777380624f69770e04ca5833
sebabustos <seba_b@outlook.com>
2013-09-17 09:06:42 -0300

    Generalización del ShowCourtain
    Se actualizó el componente ShowCourtain para generalizarlo un poco.

--- JSFramework/webSite/Betas/showCourtain.js
+++ JSFramework/webSite/Betas/showCourtain.js
@@ -1,6 +1,21 @@
-﻿function showCourtain() {
-	var courtain = $("<div id='divPostbackCourtain'></div>")
-					.addClass("ui-widget-overlay")
+﻿function showCourtain(options) {
+    var $default = {
+        sufixId: '',
+        courtainCss: 'ui-widget-overlay',
+        imageCSS: {
+            backgroundColor: "#BBBBBB",
+            zIndex: 9998
+        },
+        loadingImg: '../images/indicator.gif',
+        appendTo:'body'
+    }
+
+    var settings = $.extend({}, $default, options);
+    var courtainId = "id='divCourtain" + (settings.sufixId !== "" ? ("_" + settings.sufixId) : "");
+    var imgLoadingId = "imgLoading" + (settings.sufixId !== "" ? ("_" + settings.sufixId) : "");
+
+    var courtain = $("<div id='" + courtainId + "' courtainId='" + courtainId + "' courtainType='courtain'></div>")
+					.addClass(settings.courtainCss)
 					.css({
 						position: "absolute",
 						top: $(window).scrollTop(),
@@ -9,23 +24,28 @@
 						height: $(window).height(),
 						zIndex: 9997
 					})
-					.appendTo($("body"));
+					.appendTo($(settings.appendTo));
 
-	$("<img id='imgLoading' src='../images/indicator.gif' alt='Cargando'/>")
-		.appendTo($("body"))
-		.css({
-			backgroundColor: "#BBBBBB",
-			zIndex: 9998
-		})
+    $("<img id='" + imgLoadingId + "'  courtainId='" + courtainId + "' courtainType='loading' src='" + settings.loadingImg + "' alt='Cargando'/>")
+		.appendTo($(settings.appendTo))
+		.css(settings.imageCSS)
 		.position({
 			my: 'center center',
 			at: 'center center',
 			of: courtain
 		});
+
 	$(window).scroll(function () {
-		$("#divPostbackCourtain").css({
+	    $("[courtainId]").css({
 			top: $(window).scrollTop(),
 			left: $(window).scrollLeft()
 		});
 	});
+
+
+	return courtainId;
+}
+
+function hideCourtain(id) {
+    $("[courtainId=" + id + "]").remove();
 }
