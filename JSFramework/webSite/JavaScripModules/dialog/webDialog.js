9cdd6dbb7f97bc2f79414c4b3ddfc9a94f1dacd0
sebabustos <seba_b@outlook.com>
2014-11-06 16:05:31 -0300

    webDialog - 2014-11-06 1455 - 1.1.3.0 - Se agregó la posibilidad de configurar el ID que se utilizará para el webDialog

--- JSFramework/webSite/JavaScripModules/dialog/webDialog.js
+++ JSFramework/webSite/JavaScripModules/dialog/webDialog.js
@@ -1,155 +1,6 @@
-﻿/*
-================================================================
-VERSIÓN
-================================================================
-Código:       | webDialog - 2014-09-18 1028 - 1.1.2.0
-----------------------------------------------------------------
-Nombre:       | webDialog
-----------------------------------------------------------------
-Tipo:         | FUNCIÓN y PLUGIN
-----------------------------------------------------------------
-Descripción:  | plugin de jquery que permite mostrar en un 
-| cuadro de diálogo en el cual se podrá:
-|  - abrir una página web (iframe)
-|  - mostrar un texto
-|  - mostrar un control
-----------------------------------------------------------------
-Autor:        | Sebastián Bustos
-----------------------------------------------------------------
-Versión:      | v1.1.2.0
-----------------------------------------------------------------
-Fecha:        | 2014-09-18 10:28
-----------------------------------------------------------------
-Cambios de la Versión:
-- Se modificó para que reubique correctamente el dialog y el
-courtain al scrollar o cambiar el tamaño de la ventana contenedora
-del mismo.
-================================================================
-FUNCIONALIDADES
-================================================================
-- mostrar en un dialog una página (iframe)           (función)
-- mostrar en un dialog un texto                      (función)
-- mostrar en un dialog el contenido de un control    (plugin)
-- mostrar el cuadro de dialogo en forma modal o no.
-- dag & drop del cuadro de diálogo
-- redimensionar
-- encabezado con título (opcional) y poder mostrar un botón cerrar
-- cerrar el cuadro con "ESC"
-- estética de jquery
-- estética personalizada
-- Agregado de botones particulares.
-================================================================
-POSIBLES MEJORAS
-================================================================
--
-================================================================
-HISTORIAL DE VERSIONES
-================================================================
-Código:       | webDialog - 2014-09-12 1157 - 1.1.1.0
-Autor:        | Sebastián Bustos
-----------------------------------------------------------------
-Versión:      | v1.1.1.0
-----------------------------------------------------------------
-Fecha:        | 2014-09-12 11:57
-----------------------------------------------------------------
-Cambios de la Versión:
-- Se agregó la posiblidad de configurar el autoScroll, que 
-posicionará el cuadro de diálogo en la ubicación donde se encuentre
-scrollada la pantalla.
-- Se agregó la clase dialogTitleButtonBarContainer al anchor que 
-contiene la barra de controles
-- Se modificó el plugin para que, cuando se muestra una URL en un
-iframe, no muestre el scroll del dialog, sino sólo el del frame.
-- Se corrigió un problema que existía cuando se iniciaba el dialogo
-en una ventana pequeña, y este sobre pasaba la misma, y al agrandar
-la ventana, partes del dialogo permanecían fuera de los márgenes
-a pesar de caber. Se modificó para que en el resize de la ventana, 
-se reposicione el dialogo en el centro.
-================================================================
-Código:       | webDialog - 2014-07-21 - 1.1.0.0
-----------------------------------------------------------------
-Autor:        | Sebastián Bustos
-----------------------------------------------------------------
-Versión:      | v1.1.0.0
-----------------------------------------------------------------
-Fecha:        | 2014-07-21 10:34
-----------------------------------------------------------------
-Cambios de la Versión:
-- Se corrigió una falla que existía cuando se llamaba al plugin
-pasando como primer parámetro el texto a mostrar en el mensaje
-y como segundo la configuración. La misma no estaba almacenándose
-en el componente para luego recuperarla.
-- Se cambió el nombre del atributo usado para almacenar la 
-configuració, de "webDialog:config" a "webDialog_config"
-================================================================
-Código:       | webDialog - 2012-09-20 1730 - 1.0.0.5
-----------------------------------------------------------------
-Autor:        | Sebastián Bustos
-----------------------------------------------------------------
-Versión:      | v1.0.0.5
-----------------------------------------------------------------
-Fecha:        | 2013-06-17 16:36
-----------------------------------------------------------------
-Cambios de la Versión:
-- Se agregó la devolución del control de dialog al ejecutarse
-el método, para que sea posible, por ejemplo, almacenar en una
-variable el control, y cerrarlo a demanda.
-- Se agregó, al div de cortina, la clase divDialogCourtain
-----------------------------------------------------------------
-Código:       | webDialog - 2012-09-20 1730 - 1.0.0.4
-----------------------------------------------------------------
-Autor:        | Sebastián Bustos
-----------------------------------------------------------------
-Versión:      | v1.0.0.4
-----------------------------------------------------------------
-Fecha:        | 2012-09-29 17:30
-----------------------------------------------------------------
-Cambios de la Versión:
-- Se agregó la posibilidad, al llamar manualmente al método Close, 
-definir una función a ejecutarse luego del cierre del dialog. 
-Esta función se ejecutará antes del onCloseFunction configurado
-y podrá cancelar la ejecución de este si retorna false.
-================================================================
-Código:       | webDialog - 2012-09-20 1210 - 1.0.0.3
-----------------------------------------------------------------
-Autor:        | Sebastián Bustos
-----------------------------------------------------------------
-Versión:      | v1.0.0.3
-----------------------------------------------------------------
-Fecha:        | 2012-09-20 12:10
-----------------------------------------------------------------
-Cambios de la Versión:
-- Cuando se utiliza el tipo "control" (por ej. se utiliza el 
-plugin de jquery) al cerrar el diálogo se quitan los atributos
-en el control subyancente. Esto soluciona un problema que se daba
-al abrir y cerrar varias veces el mismo diálogo, por el cual
-quedaba visible el cuadro de diálogo.
-================================================================
-Código:       webDialog - 2012-05-22 1031 - 1.0.0.2
-Autor:        Sebastián Bustos
-Cambios de la Versión:
-- Se agregó la posibilidad de, cuando el primer parámetro es un 
-string, que el segundo sean las opciones.
-- Se agregó la posibilidad de no mostrar el botón de cierre de 
-la ventana.
-================================================================
-Código:       webDialog - 2012-04-13 1314 - 1.0.0.1
-Autor:        Sebastián Bustos
-Cambios de la Versión:
-- Se agregó tamaño porcentual al title, body y footer del dialog
-para asegurar que no se soslapen los divs
-- Se quitó el "alsoResize" del resizable porque soslapaba el 
-iframe con los otros divs.
-- se habilitó el "iframeFix" del draggable para evitar que se tilde
-con el iframe.
-================================================================
-Código:       webDialog - 2012-02-06 1347 - 1.0.0.0
-Autor:        Sebastián Bustos
-Cambios de la Versión:
-- Primera Versión
-================================================================*/
-(function ($) {
+﻿(function ($) {
     var $default = {
+        dialogId: null,
         content: null,
         position: "center",
         type: "text", // text | url | control --> dentro del dialog muestra el control indicado en content.
@@ -202,34 +53,36 @@ Cambios de la Versión:
     var webDialogsIds = 0;
     //función iframe y text
     $.webDialog = {
+        
         close: function (id) {
-            var dialogWindow = $("[webDialogId='" + id + "']");
-            var diagCont = $(".webDialogContent", dialogWindow);
-            var settings = $.extend({}, $default, dialogWindow.data("webDialog_config"));
 
-            if ((diagCont.attr("webDialog_type") == "control") && (diagCont.attr("webDialog_cloned") == "false")) {
-                $("[webDialog_ctrlId]", diagCont)
-                    .removeAttr("webDialog_ctrlId")
-                    .removeAttr("parentWebDialogId")
-                    .appendTo($("body"))
-                    .hide();
+            if (typeof id !== "undefined" && id !== null) {
+                var dialogWindow = $("[webDialogId='" + id + "']");
+                var diagCont = $(".webDialogContent", dialogWindow);
+                var settings = $.extend({}, $default, dialogWindow.data("webDialog_config"));
+
+                if ((diagCont.attr("webDialog_type") == "control") && (diagCont.attr("webDialog_cloned") == "false")) {
+                    $("[webDialog_ctrlId]", diagCont)
+                        .removeAttr("webDialog_ctrlId")
+                        .removeAttr("parentWebDialogId")
+                        .appendTo($("body"))
+                        .hide();
+                }
+                diagCont = null;
+                $("[webDialogId='divDialogCourtain_" + id + "']").remove();
+                $("[webDialogId='iframe_" + id + "']", dialogWindow).remove();
+                dialogWindow.remove();
+                var result = true;
+                if (arguments.length > 1 && typeof arguments[1] === "function") {
+                    result = arguments[1]();
+                    if (result !== false)
+                        result = true;
+                }
+                if (typeof settings.onCloseFunction != "undefined" && settings.onCloseFunction != null && result)
+                    settings.onCloseFunction();
             }
-            diagCont = null;
-            $("[webDialogId='divDialogCourtain_" + id + "']").remove();
-            $("[webDialogId='iframe_" + id + "']", dialogWindow).remove();
-            dialogWindow.remove();
-            var result = true;
-            if (arguments.length > 1 && typeof arguments[1] === "function") {
-                result = arguments[1]();
-                if (result !== false)
-                    result = true;
-            }
-            if (typeof settings.onCloseFunction != "undefined" && settings.onCloseFunction != null && result)
-                settings.onCloseFunction();
         },
         show: function (options) {
-            //$.webDialog.close();
-
             var settings = $.extend({}, $default);
 
             //Si el parámetro es un objeto se asume que es la configuración
@@ -263,8 +116,12 @@ Cambios de la Versión:
 
 
             var currentId = null;
-            currentId = webDialogsIds;
-            webDialogsIds++;
+            if (typeof settings.dialogId !== "undefined" && settings.dialogId !== null)
+                currentId = settings.dialogId;
+            else {
+                currentId = webDialogsIds;
+                webDialogsIds++;
+            }
 
             //Se crea el div del dialog mismo.
             var dialogContainer = $("<div id='divDialogWindow_" + currentId + "' webDialogId='" + currentId + "' title='" + settings.title + "' class='modalDialog'></div>");
@@ -450,13 +307,167 @@ Cambios de la Versión:
                 return this;
             }
         }
-
-
-
+        //si se llama al plugin, se inicializa el control original
         settings.content = this;
         settings.type = "control";
-
         $.webDialog.show(settings);
         return this;
     }
 })(jQuery);
+/*
+================================================================
+                           VERSIÓN
+================================================================
+Código:       | webDialog - 2014-11-06 1455 - 1.1.3.0
+----------------------------------------------------------------
+Nombre:       | webDialog
+----------------------------------------------------------------
+Tipo:         | FUNCIÓN y PLUGIN
+----------------------------------------------------------------
+Descripción:  | plugin de jquery que permite mostrar en un 
+| cuadro de diálogo en el cual se podrá:
+|  - abrir una página web (iframe)
+|  - mostrar un texto
+|  - mostrar un control
+----------------------------------------------------------------
+Autor:        | Sebastián Bustos
+----------------------------------------------------------------
+Versión:      | v1.1.3.0
+----------------------------------------------------------------
+Fecha:        | 2014-09-18 10:28
+----------------------------------------------------------------
+Cambios de la Versión:
+- Se agregó la posibilidad de configurar el ID que se utilizará
+para el webDialog
+================================================================
+FUNCIONALIDADES
+================================================================
+- mostrar en un dialog una página (iframe)           (función)
+- mostrar en un dialog un texto                      (función)
+- mostrar en un dialog el contenido de un control    (plugin)
+- mostrar el cuadro de dialogo en forma modal o no.
+- dag & drop del cuadro de diálogo
+- redimensionar
+- encabezado con título (opcional) y poder mostrar un botón cerrar
+- cerrar el cuadro con "ESC"
+- estética de jquery
+- estética personalizada
+- Agregado de botones particulares.
+================================================================
+POSIBLES MEJORAS
+================================================================
+-
+================================================================
+HISTORIAL DE VERSIONES
+================================================================
+Código:       | webDialog - 2014-09-18 1028 - 1.1.2.0
+Autor:        | Sebastián Bustos
+----------------------------------------------------------------
+Cambios de la Versión:
+- Se modificó para que reubique correctamente el dialog y el
+courtain al scrollar o cambiar el tamaño de la ventana contenedora
+del mismo.
+================================================================
+Código:       | webDialog - 2014-09-12 1157 - 1.1.1.0
+Autor:        | Sebastián Bustos
+----------------------------------------------------------------
+Versión:      | v1.1.1.0
+----------------------------------------------------------------
+Fecha:        | 2014-09-12 11:57
+----------------------------------------------------------------
+Cambios de la Versión:
+- Se agregó la posiblidad de configurar el autoScroll, que 
+posicionará el cuadro de diálogo en la ubicación donde se encuentre
+scrollada la pantalla.
+- Se agregó la clase dialogTitleButtonBarContainer al anchor que 
+contiene la barra de controles
+- Se modificó el plugin para que, cuando se muestra una URL en un
+iframe, no muestre el scroll del dialog, sino sólo el del frame.
+- Se corrigió un problema que existía cuando se iniciaba el dialogo
+en una ventana pequeña, y este sobre pasaba la misma, y al agrandar
+la ventana, partes del dialogo permanecían fuera de los márgenes
+a pesar de caber. Se modificó para que en el resize de la ventana, 
+se reposicione el dialogo en el centro.
+================================================================
+Código:       | webDialog - 2014-07-21 - 1.1.0.0
+----------------------------------------------------------------
+Autor:        | Sebastián Bustos
+----------------------------------------------------------------
+Versión:      | v1.1.0.0
+----------------------------------------------------------------
+Fecha:        | 2014-07-21 10:34
+----------------------------------------------------------------
+Cambios de la Versión:
+- Se corrigió una falla que existía cuando se llamaba al plugin
+pasando como primer parámetro el texto a mostrar en el mensaje
+y como segundo la configuración. La misma no estaba almacenándose
+en el componente para luego recuperarla.
+- Se cambió el nombre del atributo usado para almacenar la 
+configuració, de "webDialog:config" a "webDialog_config"
+================================================================
+Código:       | webDialog - 2012-09-20 1730 - 1.0.0.5
+----------------------------------------------------------------
+Autor:        | Sebastián Bustos
+----------------------------------------------------------------
+Versión:      | v1.0.0.5
+----------------------------------------------------------------
+Fecha:        | 2013-06-17 16:36
+----------------------------------------------------------------
+Cambios de la Versión:
+- Se agregó la devolución del control de dialog al ejecutarse
+el método, para que sea posible, por ejemplo, almacenar en una
+variable el control, y cerrarlo a demanda.
+- Se agregó, al div de cortina, la clase divDialogCourtain
+----------------------------------------------------------------
+Código:       | webDialog - 2012-09-20 1730 - 1.0.0.4
+----------------------------------------------------------------
+Autor:        | Sebastián Bustos
+----------------------------------------------------------------
+Versión:      | v1.0.0.4
+----------------------------------------------------------------
+Fecha:        | 2012-09-29 17:30
+----------------------------------------------------------------
+Cambios de la Versión:
+- Se agregó la posibilidad, al llamar manualmente al método Close, 
+definir una función a ejecutarse luego del cierre del dialog. 
+Esta función se ejecutará antes del onCloseFunction configurado
+y podrá cancelar la ejecución de este si retorna false.
+================================================================
+Código:       | webDialog - 2012-09-20 1210 - 1.0.0.3
+----------------------------------------------------------------
+Autor:        | Sebastián Bustos
+----------------------------------------------------------------
+Versión:      | v1.0.0.3
+----------------------------------------------------------------
+Fecha:        | 2012-09-20 12:10
+----------------------------------------------------------------
+Cambios de la Versión:
+- Cuando se utiliza el tipo "control" (por ej. se utiliza el 
+plugin de jquery) al cerrar el diálogo se quitan los atributos
+en el control subyancente. Esto soluciona un problema que se daba
+al abrir y cerrar varias veces el mismo diálogo, por el cual
+quedaba visible el cuadro de diálogo.
+================================================================
+Código:       webDialog - 2012-05-22 1031 - 1.0.0.2
+Autor:        Sebastián Bustos
+Cambios de la Versión:
+- Se agregó la posibilidad de, cuando el primer parámetro es un 
+string, que el segundo sean las opciones.
+- Se agregó la posibilidad de no mostrar el botón de cierre de 
+la ventana.
+================================================================
+Código:       webDialog - 2012-04-13 1314 - 1.0.0.1
+Autor:        Sebastián Bustos
+Cambios de la Versión:
+- Se agregó tamaño porcentual al title, body y footer del dialog
+para asegurar que no se soslapen los divs
+- Se quitó el "alsoResize" del resizable porque soslapaba el 
+iframe con los otros divs.
+- se habilitó el "iframeFix" del draggable para evitar que se tilde
+con el iframe.
+================================================================
+Código:       webDialog - 2012-02-06 1347 - 1.0.0.0
+Autor:        Sebastián Bustos
+Cambios de la Versión:
+- Primera Versión
+================================================================*/
